465a5469b9f063cf20aaf603d46e8f4de2f72747
Franky Liu <cuifranky95@gmail.com>
2019-07-17 00:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if(string.toLowerCase() === string)
+  if(string.toLowerCase() === string){
+    return "I can't hear you!"
+  }
   
 }
